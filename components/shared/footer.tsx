6457f21b385d021cac8b7740feb58f190340e63e
Mashrab Yo'ldoshev <mashrabyoldoshev@Mashrabs-MacBook-Air.local>
2024-08-26 12:25:44 +0500

    Added author username to footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="border-t mt-auto">
       <div className="container mx-auto px-4 py-6 text-center text-muted-foreground">
-        © 2024 MyShop. All rights reserved.
+        © 2024 Shopify. Created by{" "}
+        <Link href={"https://t.me/MashrabYoldashov"} target="_blank">
+          @MashrabYoldashov
+        </Link>
+        .
       </div>
     </footer>
   );
